c0eb4c28e7d253d3ba5845367eea89b7fbbf2341
Ivan Vasilev <tor419@yandex.ru>
2023-08-22 11:15:03 +0700

    One commit

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -1,4 +1,19 @@
 package ru.netology.delivery.test;
 
+import com.codeborne.selenide.Configuration;
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.Selenide.open;
+
 public class DeliveryTest {
+    private static Faker faker;
+
+
+    @BeforeEach
+    void setup() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1800x1200";
+        open("http://localhost:9999/");
+    }
 }
